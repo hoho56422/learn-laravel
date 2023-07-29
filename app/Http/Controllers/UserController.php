ebde534ffa425fdad77e37fb18559e4543fd48f2
hoho56422 <annie10731303@gmail.com>
2023-07-29 23:09:11 +0800

    add user-manager show, update & update index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Laravel\Jetstream\Jetstream;
+use App\Actions\Fortify\PasswordValidationRules;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
+    use PasswordValidationRules;
     /**
      * Display a listing of the resource.
      */
@@ -29,16 +37,30 @@ class UserController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    Validator::make($request->all(), [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => $this->passwordRules()
+    ])->validate();
+
+    User::create([
+        'name' => $request['name'],
+        'email' => $request['email'],
+        'password' => Hash::make($request['password']),
+    ]);
+
+    return redirect()->route('user-manager.index');
+}
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+        $user = User::find($id);
+        $parameters = ['user' => $user];
+        return view('user-manager.show', $parameters);
     }
 
     /**
@@ -46,7 +68,9 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $user = User::find($id);
+        $parameters = ['user' => $user];
+        return view('user-manager.edit', $parameters);
     }
 
     /**
@@ -54,7 +78,16 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+
+        Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+        ])->validate();
+    
+        $user->update($request->all());
+    
+        return redirect()->route('user-manager.show', $user->id);
     }
 
     /**
